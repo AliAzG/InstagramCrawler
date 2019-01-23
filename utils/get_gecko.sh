0ba848b4922740f95bded8d10f9e7fcd712b67eb
Ali Aminzadeh Gohari <miscofield92@gmail.com>
2019-01-23 14:19:09 +0330

    gecko.sh version updated

--- utils/get_gecko.sh
+++ utils/get_gecko.sh
@@ -2,6 +2,6 @@
 
 # following travis.yml https://github.com/iammrhelo/InstagramCrawler/blob/master/.travis.yml#L11-L14
 
-wget https://github.com/mozilla/geckodriver/releases/download/v0.16.0/geckodriver-v0.23.0-linux64.tar.gz
+wget https://github.com/mozilla/geckodriver/releases/download/v0.23.0/geckodriver-v0.23.0-linux64.tar.gz
 mkdir -p geckodriver && tar zxvf geckodriver-v0.23.0-linux64.tar.gz -C geckodriver
 
